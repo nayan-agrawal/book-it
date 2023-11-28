28bfa303be2e90f5fa48c5f297de2ba31ed56321
nayan-agrawal <nayanagrawal939@gmail.com>
2023-11-29 03:15:52 +0530

    BIT-I12 >> Updating configurations

--- next.config.js
+++ next.config.js
@@ -2,25 +2,25 @@
 const nextConfig = {
   env: {
     API_URL: "http://localhost:3000",
-    DB_LOCAL_URI: "mongodb://127.0.0.1:27017/bookit-v2",
-    DB_URI: "",
+    DB_LOCAL_URI: "mongodb+srv://nayan:crAkcpwx98xR9Bq9@bookit.ubqjygm.mongodb.net/",
+    DB_URI: "mongodb+srv://nayan:crAkcpwx98xR9Bq9@bookit.ubqjygm.mongodb.net/",
 
-    STRIPE_WEBHOOK_SECRET: "",
+    STRIPE_WEBHOOK_SECRET: "whsec_981f603424ae6a8d5e7ca8c6851bdc7351e1c4d38989c45dea1d98466d45fef4",
 
-    STRIPE_SECRET_KEY: "",
+    STRIPE_SECRET_KEY: "sk_test_51Hu1mXKnau85Qpko2ZaCexs82DRp6xMOvNnIfjfRG6oTyNr3fHtTUfizNxm2xDZQPCdH7xZu6OnCmWYt0WiySnwo00DULtinao",
 
-    CLOUDINARY_CLOUD_NAME: "",
-    CLOUDINARY_API_KEY: "",
-    CLOUDINARY_API_SECRET: "",
+    CLOUDINARY_CLOUD_NAME: "ddwquk4p9",
+    CLOUDINARY_API_KEY: "625626343342268",
+    CLOUDINARY_API_SECRET: "xecUMQ8ur7QVwxJvgC0nxaHWEtY",
 
     SMTP_HOST: "sandbox.smtp.mailtrap.io",
     SMTP_PORT: 2525,
-    SMTP_USER: "",
-    SMTP_PASSWORD: "",
+    SMTP_USER: "2ee72b74aa3410",
+    SMTP_PASSWORD: "90ff27b0c39199",
     SMTP_FROM_EMAIL: "noreply@bookit.com",
     SMTP_FROM_NAME: "BookIT",
 
-    GEOCODER_API_KEY: "",
+    GEOCODER_API_KEY: "cnE46vuIZluc7xOFGCwklFRjbVRzet2j",
     GEOCODER_PROVIDER: "mapquest",
 
     MAPBOX_ACCESS_TOKEN: "",
